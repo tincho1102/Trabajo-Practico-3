cfa2cd77cb91084af8b457863ff70e980edba286
Martín Di Marco <74630043+tincho1102@users.noreply.github.com>
2021-02-13 15:31:32 -0300

    Views created (#1)

--- tp-3/src/App.vue
+++ tp-3/src/App.vue
@@ -2,7 +2,10 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/Argentina">Argentina</router-link>
+      <router-link to="/América">América</router-link>
+      <router-link to="/Internacional">Internacional</router-link>
+      <router-link to="/Extra">Extra</router-link>
     </div>
     <router-view/>
   </div>
