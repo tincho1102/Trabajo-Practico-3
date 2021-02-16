85ccda1fc1add89f0624c700ef86c7859b13cba3
Martín Di Marco <74630043+tincho1102@users.noreply.github.com>
2021-02-15 21:38:48 -0300

    Header created (#3)
    * delete package-lock
    
    * Header Created

--- tp-3/src/views/Home.vue
+++ tp-3/src/views/Home.vue
@@ -1,14 +1,15 @@
 <template>
-  <div class="home">
+  <div>
   </div>
 </template>
-
 <script>
 
 export default {
-  name: 'Home',
   components: {
-    
+
   }
 }
 </script>
+<style>
+  
+</style>
