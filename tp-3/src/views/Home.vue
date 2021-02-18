623de08d4f627e24aaaa2f81718ec3d0949347ec
Martín Di Marco <74630043+tincho1102@users.noreply.github.com>
2021-02-18 12:38:18 -0300

    Carousel created (#4)
    * delete package-lock
    
    * Header Created
    
    * Carousel created

--- tp-3/src/views/Home.vue
+++ tp-3/src/views/Home.vue
@@ -1,12 +1,16 @@
 <template>
   <div>
+ branch-DiMarco
+    <Carousel></Carousel>
+
   </div>
 </template>
 <script>
-
+import Carousel from '../components/Carousel'
 export default {
   components: {
-
+ branch-DiMarco
+    Carousel
   }
 }
 </script>
