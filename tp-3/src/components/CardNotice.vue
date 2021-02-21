f8575557b6ab3c4fceb69494f9cea74fd0a5990a
tincho1102 <tinchodimarco11@gmail.com>
2021-02-21 12:55:31 -0300

    Modals

---
+++ tp-3/src/components/CardNotice.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="card" style="width: 18rem;">
+        <img src="../assets/iconos/DOLAR.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">Moneda extranjera</h5>
+            <p class="card-text">Conocé el valor de todas las monedas internacionales hoy.</p>
+            <a href="https://datosmacro.expansion.com/divisas" target="_blank" class="btn btn-primary">Click Aquí</a>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="">
+    
+</style>
--- tp-3/src/views/Extra.vue
+++ tp-3/src/views/Extra.vue
@@ -1,10 +1,19 @@
 <template>
     <div>
-        
+        <ModalElim></ModalElim>
+        <ModalEdit></ModalEdit>
+        <ModalAgregar></ModalAgregar>
     </div>
 </template>
 <script>
+import ModalElim from '../components/ModalElim.vue'
+import ModalEdit from '../components/ModalEdit.vue'
+import ModalAgregar from '../components/ModalAgregar.vue'
 export default {
-    
+    components:{
+        ModalElim,
+        ModalEdit,
+        ModalAgregar,
+    }
 }
 </script>
